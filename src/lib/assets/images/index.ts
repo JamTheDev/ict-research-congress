2efda96a05f14355fc58b0f8b1470f1b94c63a4f
JamTheDev <2004jamvillarosa@gmail.com>
2024-02-29 22:40:53 +0800

    Finished `/` page.

--- src/lib/assets/images/index.ts
+++ src/lib/assets/images/index.ts
@@ -1,8 +1,22 @@
-import hero from './hero.webp';
-import logo1 from './logo-1.png';
-import logos from './logos.webp';
-import computer1 from './computer-1.webp';
-import computer2 from './computer-2.png';
-import nodes from './10.webp';
+import hero from "./hero.webp";
+import logo1 from "./logo-1.png";
+import logos from "./logos.webp";
+import computer1 from "./computer-1.webp";
+import computer2 from "./computer-2.png";
+import nodes from "./10.webp";
 
-export { hero, logo1, computer2, computer1, logos, nodes };
+import ictrc7th from "./ictrc-7th.png";
+import ictrc8th from "./ictrc-8th.png";
+import ictrc9th from "./ictrc-9th.png";
+
+export {
+  computer1,
+  computer2,
+  hero,
+  ictrc7th,
+  ictrc8th,
+  ictrc9th,
+  logo1,
+  logos,
+  nodes,
+};
--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -10,11 +10,11 @@
 		<img src={computer1} alt="Computer" class="absolute -right-56 h-full z-[9] hidden md:block" />
 
 		<div class="w-2/3 flex flex-col py-20 gap-5 relative z-10">
-			<h1 class="font-metropolis-bold text-4xl lg:text-6xl 2xl:text-8xl uppercase">
+			<h1 class="font-metropolis-bold text-2xl lg:text-3xl 2xl:text-4xl uppercase">
 				Call for Papers
 			</h1>
 
-			<p class="text-lg lg:text-2xl 2xl:text-3xl font-metropolis-medium">
+			<p class="text-lg lg:text-xl 2xl:text-2xl font-metropolis-medium">
 				Do you have groundbreaking research pushing the boundaries of Information Technology? Share
 				your insights and innovations on the global stage at the UMak ICT Research Congress!
 			</p>
@@ -33,15 +33,19 @@
 
 		<div class="py-10 px-[5%] z-10 relative">
 			<div class="space-y-5 mb-40">
-				<div class="h-20 flex items-center gap-10">
+				<div class="h-10 flex items-center gap-5">
 					<img src={logos} alt="Makati, UMak, & CCIS" class="object-cover h-full" />
 					<img src={logo1} alt="ICT Research Congress" class="object-cover h-full" />
 				</div>
 
 				<div class="space-y-2">
-					<p class="uppercase text-base">City Government of Makati - Republic of the Philippines</p>
-					<p class="uppercase text-4xl font-marcellus">University of Makati</p>
-					<p class="text-base font-marcellus">College of Computing and Information Sciences</p>
+					<p class="uppercase font-metropolis tracking-widest text-xs">
+						City Government of Makati - Republic of the Philippines
+					</p>
+					<div>
+						<p class="uppercase text-3xl font-marcellus"><span class="text-[2.0rem] font-marcellus">U</span>niversity of <span class="text-[2.0rem] font-marcellus">M</span>akati</p>
+						<p class="text-xs font-marcellus">College of Computing and Information Sciences</p>
+					</div>
 				</div>
 			</div>
 
@@ -52,12 +56,11 @@
 					target="_blank"
 					rel="noreferrer"
 				>
-					<Facebook class="w-10 h-10" />
-					<p class="text-lg lg:text-2xl font-metropolis-medium">/ ICT Research Congress</p>
+					<Facebook class="w-5 h-5" />
+					<p class="text-lg lg:text-lg font-metropolis-medium">/ ICT Research Congress</p>
 				</a>
 
-				<a href="mailto:ccis@umak.edu.ph" class="text-lg lg:text-2xl font-metropolis-medium"
-					>ccis@umak.edu.ph</a
+				<a href="mailto:ccis@umak.edu.ph" class="text-md font-metropolis-medium">ccis@umak.edu.ph</a
 				>
 			</div>
 		</div>
