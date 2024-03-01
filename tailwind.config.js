b67493c87975ee006076df04f5a81d15e7064893
xGihyun <giordnnuz@gmail.com>
2024-03-01 12:48:33 +0800

    Fixed bg image 10.webp not found

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ const config = {
 
 		extend: {
 			backgroundImage: {
-				nodes: "url('./src/lib/assets/images/10.webp')"
+				nodes: "url('./lib/assets/images/10.webp')"
 			},
 			boxShadow: {
 				'md-2': '0 4px 10px -1px rgb(0 0 0 / 0.3)'
