ff23c15487829c0bec6a71a06eec505fc848f366
JamTheDev <2004jamvillarosa@gmail.com>
2024-03-22 11:38:17 +0800

    added lazy image component

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,3 +1,17 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import LazyImage from "$lib/components/lazy-image.svelte";
 
-<div class="px-[5%] container py-20">insert previous ICT RC and other images</div>
+
+</script>
+
+
+<main class="py-[6%] px-[5%] container">
+
+    <h1 class="font-metropolis-black text-3xl pt-5 pb-3 text-center">Previous ICT Research Congress</h1>
+    <div class="w-full bg-gradient-to-l from-primary to-secondary h-1"></div>
+
+    <div class="flex flex-row flex-wrap w-full gap-10">
+        <LazyImage className="flex-[33%] h-52" src="" />
+        <LazyImage className="flex-[60%] h-52" src="" />
+    </div>
+</main>
